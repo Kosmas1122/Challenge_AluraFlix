be98b1f3b5a0bdc3faac585889c6e843b3182753
David-Zephyrus <dbekosmas@gmail.com>
2025-01-07 20:19:16 -0500

    Crea esqueleto de <Footer />.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Header from './components/Header';
 import GlobalStyles from './components/GlobalStyles';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
