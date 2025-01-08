945cfa0a1920dffae11898a24cd607be660e9a52
David-Zephyrus <dbekosmas@gmail.com>
2025-01-08 14:26:40 -0500

    Se crea esqueleto de <Modal/>.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+
+const ModalStyled = styled.div`
+	height: 400px;
+	width: 600px;
+	background-color: #7fc6e9;
+	border: 1px solid white;
+`;
+
 export default function Modal() {
-  return <div>Modal</div>;
+	return <ModalStyled> MODAL </ModalStyled>;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Banner from "../components/Banner";
 import Categorias from "../components/Categorias";
+import Modal from "../components/Modal";
+
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <Categorias>
-        
-      </Categorias>
+      <Categorias />
+      <Modal />
     </>
   );
 }
