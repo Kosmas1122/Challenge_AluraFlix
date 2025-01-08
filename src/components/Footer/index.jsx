8abd227e95ae446e9fb75b779cd4083510002750
David-Zephyrus <dbekosmas@gmail.com>
2025-01-07 20:58:48 -0500

    Se implementa el <Footer />.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+
+const FooterStyled = styled.footer`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  align-self: flex-end;
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  height: 125px;
+  border: 1px solid #2271d1;
+  background: rgba(0, 0, 0, 0.9);
+  background-blend-mode: normal;
+  box-shadow: 0px 5px 29px rgba(34, 113, 209, 0.7);
+`;
+
+const LogoStyled = styled.img`
+  width: 168.45px;
+  height: 40px;
+  align-content: center;
+`;
+
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    return (
+      <FooterStyled>
+        <LogoStyled src="images/Logo_AluraFlix.png" alt="Logo Alura" />
+      </FooterStyled>
+    );
 }
