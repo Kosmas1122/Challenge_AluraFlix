8415a4300139fa29c32a737e13b70cb52f2f24aa
David-Zephyrus <dbekosmas@gmail.com>
2025-01-07 23:46:27 -0500

    Componente <Banner/> OK.

---
+++ src/components/Banner/BannerVideo/index.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+const VideoStyled = styled.iframe`
+  height: 333px;
+  width: 650px;
+  border-radius: 15px;
+  border: 5px solid #6bd1ff;
+  box-shadow: inset 0px 0px 17px 8px #6bd1ff;
+`;
+
+export default function BannerVideo() {
+  return (
+    <VideoStyled
+      src="https://www.youtube.com/embed/ov7vA5HFe6w"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></VideoStyled>
+  );
+}
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,20 +1,88 @@
 import styled from "styled-components"
+import BannerVideo from "./BannerVideo";
 
 const BannerStyled = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 832px;
   width: 100%;
   background: rgba(0, 18, 51, 0.56);
   background-blend-mode: normal;
   background-image: url("images/Banner_Fondo.png");
-  background-size: auto;
+  background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+
+  div.tarjeta {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 333px;
+    width: 100%;
+    margin: 190px 40px 0px 40px;
+    border: 1px solid blue;
+  }
+
+  div.contenido {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  h1 {
+    height: 92px;
+    width: 296px;
+    margin: 0;
+    border-radius: 15px;
+    background: #6bd1ff;
+    background-blend-mode: normal;
+    color: #f5f5f5;
+    text-align: center;
+    line-height: 92px;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    font-size: 48px;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+
+  h2 {
+    margin: 40px 0px 9px 0px;
+    color: #f5f5f5;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    font-size: 46px;
+    font-weight: 400;
+  }
+
+  p {
+    width: 662.84px;
+    height: 110px;
+    color: #f5f5f5;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    font-size: 18px;
+    font-weight: 300;
+  }
 `;
+
 
 export default function Banner() {
   return (
-      <BannerStyled>
-          
+    <BannerStyled>
+      <div className="tarjeta">
+        <div className="contenido">
+          <h1>Front end</h1>
+          <h2>Challenge React</h2>
+          <p>
+            Este challenge es una forma de aprendizaje. Es un mecanismo donde
+            podrás comprometerte en la resolución de un problema para poder
+            aplicar todos los conocimientos adquiridos en la formación React.
+          </p>
+        </div>
+        <BannerVideo />
+      </div>
     </BannerStyled>
-  )
+  );
 }
