6bbeb3fbf717942e7a3d9df338b612312a7fce55
David-Zephyrus <dbekosmas@gmail.com>
2025-01-08 15:57:14 -0500

    Se da estilos al <Modal/>.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,19 +1,46 @@
 import styled from "styled-components";
 import { useHomeContext } from "../../context/HomeContext";
 
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.75);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
 const ModalStyled = styled.div`
-  height: 400px;
-  width: 600px;
-  background-color: #7fc6e9;
-  border: 1px solid white;
+  position: absolute;
+  top: 130px;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 1140px;
+  width: 974px;
+
+  border: 5px solid #6bd1ff;
+  border-radius: 10px;
+  background: #03122f;
+  background-blend-mode: normal;
+
+  /* background-color: #7fc6e9; */
 `;
 
 export default function Modal() {
   const { isModalOpen, closeModal } = useHomeContext();
 
+  /* Sube la ventana al inicio: */
+  {window.scrollTo({ top: 0, behavior: "smooth" });}
+
   if (!isModalOpen) return null;
 
-	return <ModalStyled>
-		<button onClick={closeModal}> Close </button>
-	</ModalStyled>;
+  return (
+    <>
+      <Overlay />
+      <ModalStyled>
+        <button onClick={closeModal}> Close </button>
+      </ModalStyled>
+      ;
+    </>
+  );
 }
