f77d078a655d11cf93fcc6c64c5948a6697abdbb
David-Zephyrus <dbekosmas@gmail.com>
2025-01-09 11:21:28 -0500

    Se limpia código en <Modal/>.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -4,9 +4,8 @@ import { useState } from "react";
 
 // styled-component y Material UI:
 import styled from "styled-components";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { Select, MenuItem, InputLabel, FormControl, Button } from "@mui/material";
+import { Button } from "@mui/material";
 
 // Fuentes:
 import "@fontsource/roboto/300.css";
@@ -27,7 +26,7 @@ const Overlay = styled.div`
   bottom: 0;
 `;
 
-const ModalStyled = styled.div`
+const ModalStyled = styled.form`
   box-sizing: border-box;
   position: absolute;
   top: 130px;
@@ -46,19 +45,6 @@ const ModalStyled = styled.div`
     color: #2271d1;
   }
 
-  /* form {
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-  } */
-
-  /* div.botones {
-    padding-top: 20px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  } */
-
   .BtnCerrar {
     position: absolute;
     top: 5px;
@@ -68,14 +54,6 @@ const ModalStyled = styled.div`
 
 export default function Modal() {
   const { isModalOpen, closeModal } = useHomeContext();
-  const [selectedValue, setSelectedValue] = useState("");
-
-  /* Maneja el valor del campo Select: */
-  const handleChange = (event) => {
-    setSelectedValue(event.target.value); // Actualiza el estado con el valor seleccionado
-  };
-
-
   
   /* Sube la ventana al inicio: */
   {window.scrollTo({ top: 0, behavior: "smooth" });}
@@ -86,84 +64,7 @@ export default function Modal() {
     <>
       <Overlay />
       <ModalStyled>
-        <Typography variant="h3" align="center" component="h1">
-          Editar Card:
-        </Typography>
-
-        {/* <form>
-          <TextField
-            id="titulo"
-            label="Título"
-            variant="outlined"
-            margin="normal"
-            size="medium"
-            placeholder="Ingrese un título para el video ..."
-            fullWidth
-          />
-
-          <FormControl fullWidth>
-            <InputLabel id="cat">Categoria</InputLabel>
-            <Select
-              labelId="cat"
-              label="Categoria"
-              value={selectedValue}
-              onChange={handleChange}
-              fullWidth
-            >
-              <MenuItem value="Frontend">Frontend</MenuItem>
-              <MenuItem value="Backend">Backend</MenuItem>
-              <MenuItem value="Innovación y Gestión">
-                Innovación y Gestión
-              </MenuItem>
-            </Select>
-          </FormControl>
-
-          <TextField
-            id="imagen"
-            label="Imagen"
-            variant="outlined"
-            margin="normal"
-            size="medium"
-            placeholder="Ingrese la URL de la imagen ..."
-            fullWidth
-          />
-
-          <TextField
-            id="video"
-            label="Vídeo"
-            variant="outlined"
-            margin="normal"
-            size="medium"
-            placeholder="Ingrese la URL del video ..."
-            fullWidth
-          />
-
-          <TextField
-            id="descripcion"
-            label="Descripcion"
-            variant="outlined"
-            margin="normal"
-            size="medium"
-            placeholder="Ingrese una descripción para el video ..."
-            fullWidth
-            multiline
-            minRows={3}
-            maxRows={3}
-          />
-        </form> */}
-
-        {/* 
-        <div className="botones">
-          <Button variant="contained" size="large">
-            {" "}
-            Guardar{" "}
-          </Button>
-          <Button variant="contained" size="large">
-            {" "}
-            Limpiar{" "}
-          </Button>
-        </div> */}
-
+        <Typography variant="h3" align="center" component="h1">Editar Card</Typography>
         <Formulario />
         <Button className="BtnCerrar" variant="contained" size="large" onClick={closeModal}>Cerrar</Button>
       </ModalStyled>
