3dbea9b3fea5e4596fede3da7cc0d6f83127ec86
David-Zephyrus <dbekosmas@gmail.com>
2025-01-08 20:19:29 -0500

    <Modal/> OK.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 import { useHomeContext } from "../../context/HomeContext";
+import { useState } from "react";
+
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { Select, MenuItem, InputLabel, FormControl, Button } from "@mui/material";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 
 const Overlay = styled.div`
   background-color: rgba(0, 0, 0, 0.75);
@@ -11,26 +22,57 @@ const Overlay = styled.div`
 `;
 
 const ModalStyled = styled.div`
+  box-sizing: border-box;
   position: absolute;
   top: 130px;
   left: 50%;
   transform: translateX(-50%);
-  height: 1140px;
-  width: 974px;
+  height: 720px;
+  width: 774px;
+  /* height: 1140px; */
+  /* width: 974px; */
 
+  padding: 0px 100px;
   border: 5px solid #6bd1ff;
   border-radius: 10px;
-  background: #03122f;
+  background: #dcf3ff;
   background-blend-mode: normal;
+  /* background: #03122f; */
+  /* background: #c6e3f5da; */
 
-  /* background-color: #7fc6e9; */
+  h1 {
+    margin: 40px 0px 20px 0px;
+    color: #2271d1;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+
+  div.botones {
+    padding-top: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
 export default function Modal() {
   const { isModalOpen, closeModal } = useHomeContext();
+  const [selectedValue, setSelectedValue] = useState("");
+
+  /* Maneja el valor del campo Select: */
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value); // Actualiza el estado con el valor seleccionado
+  };
+
 
   /* Sube la ventana al inicio: */
-  {window.scrollTo({ top: 0, behavior: "smooth" });}
+  {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
 
   if (!isModalOpen) return null;
 
@@ -38,7 +80,86 @@ export default function Modal() {
     <>
       <Overlay />
       <ModalStyled>
-        <button onClick={closeModal}> Close </button>
+        <Typography variant="h3" align="center" component="h1">
+          Editar Card:
+        </Typography>
+
+        <form>
+          <TextField
+            id="titulo"
+            label="Título"
+            variant="outlined"
+            margin="normal"
+            size="medium"
+            placeholder="Ingrese un título para el video ..."
+            fullWidth
+          />
+
+          <FormControl fullWidth>
+            <InputLabel id="cat">Categoria</InputLabel>
+            <Select
+              labelId="cat"
+              label="Categoria"
+              value={selectedValue}
+              onChange={handleChange}
+              fullWidth
+            >
+              <MenuItem value="Frontend">Frontend</MenuItem>
+              <MenuItem value="Backend">Backend</MenuItem>
+              <MenuItem value="Innovación y Gestión">
+                Innovación y Gestión
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField
+            id="imagen"
+            label="Imagen"
+            variant="outlined"
+            margin="normal"
+            size="medium"
+            placeholder="Ingrese la URL de la imagen ..."
+            fullWidth
+          />
+
+          <TextField
+            id="video"
+            label="Vídeo"
+            variant="outlined"
+            margin="normal"
+            size="medium"
+            placeholder="Ingrese la URL del video ..."
+            fullWidth
+          />
+
+          <TextField
+            id="descripcion"
+            label="Descripcion"
+            variant="outlined"
+            margin="normal"
+            size="medium"
+            placeholder="Ingrese una descripción para el video ..."
+            fullWidth
+            multiline
+            minRows={3}
+            maxRows={3}
+          />
+        </form>
+
+        <div className="botones">
+          <Button variant="contained" size="large">
+            {" "}
+            Guardar{" "}
+          </Button>
+          <Button variant="contained" size="large">
+            {" "}
+            Limpiar{" "}
+          </Button>
+          <Button variant="contained" size="large" onClick={closeModal}>
+            {" "}
+            Cerrar{" "}
+          </Button>
+        </div>
       </ModalStyled>
       ;
     </>
