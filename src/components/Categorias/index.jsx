13724a801d547dda1304926681bb3ecb083818fe
David-Zephyrus <dbekosmas@gmail.com>
2025-01-09 15:59:53 -0500

    Se leen datos desde json-server en <Categorias/>.

--- src/components/Categorias/index.jsx
+++ src/components/Categorias/index.jsx
@@ -1,12 +1,44 @@
 import styled from "styled-components";
 import Categoria from "../Categoria";
+import { useState, useEffect } from "react";
 
 const CategoriasStyled = styled.div`
   background: #262626;
   background-blend-mode: normal;
 `;
 
+// Se hace una petición o requisición de tipo GET:
+async function listarVideos() {
+  try {
+    const conexion = await fetch("http://localhost:3001/videos");
+    const conexionJSON = await conexion.json();
+
+    return conexionJSON;
+  }
+  catch (error) {
+    console.error("Error al obtener los videos: ", error);
+  }
+}
+
+
 function Categorias() {
+
+  const [videos, setVideos] = useState([]);
+
+  // Lee la lista de videos desde json-server:
+  useEffect(() => {
+    listarVideos()
+      .then((data) => {
+        setVideos(data);
+      })
+      .catch((error) => {
+        console.error("Error al listar los videos:", error);
+      });
+  }, []);
+
+  console.log(videos);
+
+  
   return (
     <CategoriasStyled>
       <Categoria nombre="frontend" fondo="#6bd1ff" />
