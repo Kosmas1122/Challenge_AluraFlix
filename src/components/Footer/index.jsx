fdc519cd91b4be9b4faeb452f7df779955b007e9
David-Zephyrus <dbekosmas@gmail.com>
2025-01-13 19:13:53 -0500

    <Footer/> Responsivo --> OK.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Boton from "../Boton";
+import { useState } from "react";
 
 const FooterStyled = styled.footer`
   box-sizing: border-box;
@@ -31,10 +34,52 @@ const LogoStyled = styled.img`
   }
 `;
 
+const BotonesStyled = styled.div`
+  // Phone:
+  @media (max-width: 430px) {
+    display: flex;
+    flex-direction: row;
+    gap: 25px;
+    padding-right: 30px;
+  }
+
+  // Desktops mayores:
+  @media (min-width: 431px) {
+    display: none;
+  }
+`;
+
+
 export default function Footer() {
+
+  const [botonSeleccionado, setBotonSeleccionado] = useState("home");
+  
+    function alternarSeleccion(boton) {
+      setBotonSeleccionado(boton);
+    }
+
     return (
       <FooterStyled>
         <LogoStyled src="images/Logo_AluraFlix.png" alt="Logo Alura" />
+        <BotonesStyled>
+          <Link to="/">
+            <Boton
+              texto="home"
+              estado={botonSeleccionado === "home" ? "selected" : "unSelected"}
+              onClick={() => alternarSeleccion("home")}
+            />
+          </Link>
+
+          <Link to="/NuevoVideo">
+            <Boton
+              texto="nuevo video"
+              estado={
+                botonSeleccionado === "nuevo video" ? "selected" : "unSelected"
+              }
+              onClick={() => alternarSeleccion("nuevo video")}
+            />
+          </Link>
+        </BotonesStyled>
       </FooterStyled>
     );
 }
