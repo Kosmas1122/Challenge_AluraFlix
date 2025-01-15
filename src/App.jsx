583d4005bd9245a2b8491f9742c4e8ef557928a3
David-Zephyrus <dbekosmas@gmail.com>
2025-01-15 12:05:24 -0500

    Se limpia <App />.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // Contextos:
 import { HomeProvider } from './context/HomeContext';
-import { MensajeProvider } from './context/MensajeContexto';
 
 // Páginas:
 import Home from './pages/Home';
@@ -13,23 +12,18 @@ import NuevoVideo from './pages/NuevoVideo';
 
 // Componentes:
 import Header from './components/Header';
-import Mensaje from './components/Mensaje';
-import MensajeProp from './components/MensajeProp';
-import GlobalStyles from './components/GlobalStyles';
 import Footer from './components/Footer';
 
 
 function App() {
   return (
     <Router>
-      <Header />
-      <MensajeProvider><Mensaje /></MensajeProvider>  {/* Mensaje debe estar envuelto por MensajeProvider. */}
-      <MensajeProp msg="Salud a todos!" />            {/* MensajeProp NO depende de MensajeProvider y funciona igual. */}
+      <HomeProvider><Header/></HomeProvider>
       <Routes>
         <Route path="/" element={<HomeProvider> <Home /> </HomeProvider>} />
         <Route path="/NuevoVideo" element={<NuevoVideo />} />
       </Routes>
-      <Footer />
+      <HomeProvider><Footer /></HomeProvider>
     </Router>
   );
 }
